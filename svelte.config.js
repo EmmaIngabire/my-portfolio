8a0ba7a89219d26b5c2e5f0b7b84ded519be52e5
EmmaIngabire <125594106+EmmaIngabire@users.noreply.github.com>
2024-10-16 14:13:33 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,48 @@
-import adapter from '@sveltejs/adapter-static';
+name: Deploy to GitHub Pages
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    kit: {
-        adapter: adapter({
-            pages: 'build', // This is where your output will go
-            assets: 'build',
-            fallback: null,  // Set if needed for SPA behavior
-        })
-    }
-};
+on:
+  push:
+    branches: ["main"]
 
-export default config;
+jobs:
+  build_site:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+
+      - name: Install Node.js
+        uses: actions/setup-node@v4
+        with:
+          node-version: 20
+          cache: npm
+
+      - name: Install dependencies
+        run: npm install
+
+      - name: Build
+        env:
+          BASE_PATH: "/${{ github.event.repository.name }}"  # Set the base path for GitHub Pages
+        run: |
+          npm run build  # Ensure your build outputs to the correct folder
+
+      - name: Upload Artifacts
+        uses: actions/upload-pages-artifact@v3
+        with:
+          path: ".svelte-kit/output/client"  # Update this to your actual build output path
+
+  deploy:
+    needs: build_site
+    runs-on: ubuntu-latest
+    permissions:
+      pages: write
+      id-token: write
+
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+
+    steps:
+      - name: Deploy
+        id: deployment
+        uses: actions/deploy-pages@v4

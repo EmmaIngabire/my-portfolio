c3064a203d8125f1e934c472c65cf5d8162f3ff4
EmmaIngabire <125594106+EmmaIngabire@users.noreply.github.com>
2024-10-16 17:39:08 -0400

    Update +page.svelte

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -3,17 +3,15 @@
 </script>
 
 <svelte:head>
-	<title> Projects</title>
+    <title>Projects</title>
 </svelte:head>
 
 <div class="projects">
-{#each projects as p}
-    <article>
-        <h2>Commodi nulla veritatis iure!</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Assumenda voluptatem excepturi veritatis debitis nulla repellat obcaecati officia ipsa amet 
-laboriosam voluptatibus, quod tempore laudantium aut, quasi saepe minima natus. Reprehenderit sit nulla recusandae 
-est doloremque asperiores alias neque.</p>
-    </article>
-{/each}
+    {#each projects as p}
+        <article>
+            <h2>{p.title}</h2>
+            <img src={p.image} alt={p.title}>
+            <p>{p.description}</p>
+        </article>
+    {/each}
 </div>

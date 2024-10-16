63037afec13eca0dbdca7f392f3d167df5324295
EmmaIngabire <125594106+EmmaIngabire@users.noreply.github.com>
2024-10-16 18:37:36 -0400

    Update +page.svelte

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -5,7 +5,6 @@
 <svelte:head>
     <title>Projects</title>
 </svelte:head>
-<h1 style="margin-bottom: 0;>Total Projects: {projects.length}</h1>
 <div class="projects">
     {#each projects as p}
         <article>

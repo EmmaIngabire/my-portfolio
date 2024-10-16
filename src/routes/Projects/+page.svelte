54f112b099ead06d5e42b33481ba2fbfe95384c9
EmmaIngabire <125594106+EmmaIngabire@users.noreply.github.com>
2024-10-16 17:31:09 -0400

    Update +page.svelte

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -2,8 +2,6 @@
     import projects from '$lib/projects.json';
 </script>
 
-<pre>{ JSON.stringify(projects, null, "\t") }</pre>
-
 <svelte:head>
 	<title> Projects</title>
 </svelte:head>
@@ -13,7 +11,9 @@
     <article>
         <h2>Commodi nulla veritatis iure!</h2>
         <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Assumenda voluptatem excepturi veritatis debitis nulla repellat obcaecati officia ipsa amet laboriosam voluptatibus, quod tempore laudantium aut, quasi saepe minima natus. Reprehenderit sit nulla recusandae est doloremque asperiores alias neque.</p>
+        <p>Assumenda voluptatem excepturi veritatis debitis nulla repellat obcaecati officia ipsa amet 
+laboriosam voluptatibus, quod tempore laudantium aut, quasi saepe minima natus. Reprehenderit sit nulla recusandae 
+est doloremque asperiores alias neque.</p>
     </article>
 {/each}
 </div>

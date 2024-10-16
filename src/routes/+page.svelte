268ad699ecdb4780ee5702652aab42dd7f5f3b95
Emma <eingabir@andrew.cmu.edu>
2024-10-16 19:06:57 -0400

    thtr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Home</title>
 </svelte:head>
 
-<h2>Summary</h2><br>
+<h1>Hi, I am Emma</h1><br>
  <p> I am a data scientist professional with expertise in data collection, analysis, and impact evaluation. My skill set includes geospatial information
 systems (GIS), statistical analysis, and data visualization using tools like Python, R, and Tableau. I have hands-on experience in conducting and 
 managing both quantitative and qualitative research, and I am adept at using digital data collection tools. I am passionate about using data to 
@@ -12,3 +12,21 @@ eager to leverage my analytical skills to support effective impact monitoring an
  src="Images/Emma_image.jpg" 
   alt = "Emma's passport photo"
   />
+
+  <script>
+    import projects from '$lib/projects.json';  // Import the projects data
+    import Project from '$lib/Project.svelte';  // Import the Project component
+</script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+<h2>Latest Projects</h2> <!-- Add a heading for the section -->
+
+<div class="projects highlights"> <!-- Wrap the projects in a div for styling -->
+    {#each projects.slice(0, 3) as p} <!-- Use slice to get the first 3 projects -->
+        <Project data={p} /> <!-- Use the Project component -->
+    {/each}
+</div>
+    

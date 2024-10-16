80bba3ab1c48c4ec4407376582998c52e51c4215
EmmaIngabire <125594106+EmmaIngabire@users.noreply.github.com>
2024-10-16 17:23:32 -0400

    Update +page.svelte

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -9,62 +9,11 @@
 </svelte:head>
 
 <div class="projects">
-    <article>
-        <a href="https://emmaingabire.github.io/portfolio/" >Assignment 1</a>
-    </article>
-    <article>
-        <h2>Maxime alias nam quia.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Exercitationem fugit tenetur sit. Labore deserunt accusantium quos saepe neque voluptatem, amet, aliquam repellat praesentium maxime architecto. Et dicta ipsum quod facilis perspiciatis, excepturi odio illum sequi animi cum fuga.</p>
-    </article>
-    <article>
-        <h2>Alias soluta deserunt consectetur.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Nemo dolore quis ipsa, eos facilis dolorum voluptatibus magnam labore, sit corrupti consectetur animi illo non a enim beatae nisi explicabo eligendi vitae ullam corporis, dolores perferendis voluptatem. Laborum, minima!</p>
-    </article>
-    <article>
-        <h2>Sunt quam exercitationem ipsa.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Suscipit hic non incidunt atque rem tenetur nulla vitae quasi harum possimus, beatae iste ducimus praesentium dolore eos omnis! Similique corporis aut aliquam ratione nesciunt! Nesciunt iste pariatur recusandae maiores?</p>
-    </article>
-    <article>
-        <h2>Quisquam molestias sapiente alias.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Sunt velit officiis libero aspernatur laboriosam. Natus doloremque non in omnis recusandae cum esse! Necessitatibus laudantium reprehenderit unde sit quisquam. Ipsa, sed velit et corrupti recusandae repellat harum ducimus illum?</p>
-    </article>
-    <article>
-        <h2>Assumenda iusto nostrum rem.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Hic adipisci, nesciunt necessitatibus quaerat sequi sapiente rem praesentium commodi natus molestiae veritatis fugiat harum asperiores debitis itaque exercitationem suscipit ipsa sit tempore distinctio earum voluptatum voluptatibus? Cumque, magni officia!</p>
-    </article>
-    <article>
-        <h2>Sequi tempora praesentium itaque?</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Repudiandae sapiente ad eligendi, sunt incidunt molestiae ipsum minus aut dolores cumque doloremque, impedit dolorum debitis facilis animi! Consectetur, beatae ullam. Maiores in voluptatibus id reiciendis velit! Ducimus, numquam amet?</p>
-    </article>
-    <article>
-        <h2>Ipsam accusantium nisi rerum.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Quo amet eveniet nulla enim quod eos rerum expedita nisi, maxime, soluta, accusamus dicta earum. Maxime cupiditate velit molestiae minus facere! Aspernatur nihil in debitis distinctio voluptatem consequatur recusandae asperiores.</p>
-    </article>
-    <article>
-        <h2>Beatae ea explicabo sint.</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Rem error tempore aspernatur iure aliquam a, expedita reiciendis nobis suscipit vel sint impedit animi porro excepturi magni ex cupiditate consequatur consectetur facere. Ducimus consequatur, quas aliquam odit dolorum inventore.</p>
-    </article>
-    <article>
-        <h2>Odio inventore quidem numquam?</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Adipisci molestiae similique autem in vel nobis totam. Quia reiciendis neque dolore placeat, impedit voluptate voluptates cupiditate quos provident maiores aperiam, officiis hic debitis rem eligendi tempore sequi ad quae!</p>
-    </article>
+{#each projects as p}
     <article>
         <h2>Commodi nulla veritatis iure!</h2>
         <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
         <p>Assumenda voluptatem excepturi veritatis debitis nulla repellat obcaecati officia ipsa amet laboriosam voluptatibus, quod tempore laudantium aut, quasi saepe minima natus. Reprehenderit sit nulla recusandae est doloremque asperiores alias neque.</p>
     </article>
-    <article>
-        <h2>Numquam corrupti reprehenderit perspiciatis?</h2>
-        <img src="https:/dig.cmu.edu/datavis-fall-2024/labs/2/images/empty.svg" alt="">
-        <p>Ad aspernatur magni architecto molestiae ut maiores dolorem iusto. Debitis, nobis, aperiam expedita reiciendis natus est repudiandae aliquam voluptatum necessitatibus saepe repellendus reprehenderit impedit corrupti, repellat accusamus dolor. Error, qui.</p>
-    </article>
+</each>
 </div>
